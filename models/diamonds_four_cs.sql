02307c61473dea1c5eaf16954b07ae0d1c567090
JuanDiego18 <jdiegov2324@gmail.com>
2024-11-28 09:57:44 -0600

    Removing unnecesary models

--- models/diamonds_four_cs.sql
+++
@@ -1,8 +0,0 @@
-{{ config(
-  materialized='table',
-  file_format='delta'
-) }}
-
-select carat, cut, color, clarity
-from `develoment-catalog`.`dbt-schema`.diamonds
-
--- models/diamonds_prices.sql
+++
@@ -1,4 +0,0 @@
-select color, avg(price) as price
-from `develoment-catalog`.`dbt-schema`.diamonds
-group by color
-order by price desc
