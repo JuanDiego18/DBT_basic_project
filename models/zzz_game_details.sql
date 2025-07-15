8c7b496e9fe61243c847461da0e09e6ed5f39d0f
JuanDiego18 <jdiegov2324@gmail.com>
2025-07-15 14:25:19 -0600

    Making the code reusable

--- models/zzz_game_details.sql
+++ models/zzz_game_details.sql
@@ -45,18 +45,19 @@ from (
       gs.visitor_team_score as visitor_score,
       g.game_date
     from
-      `development_catalog`.`dbt-schema`.zzz_games as g,
-      `development_catalog`.`dbt-schema`.zzz_game_opponents as go,
-      `development_catalog`.`dbt-schema`.zzz_game_scores as gs
+      `{{ var('dbt_catalog') }}`.`dbt-schema`.zzz_games as g,
+      `{{ var('dbt_catalog') }}`.`dbt-schema`.zzz_game_opponents as go,
+      `{{ var('dbt_catalog') }}`.`dbt-schema`.zzz_game_scores as gs
     where
       g.game_id = go.game_id and
       g.game_id = gs.game_id
   ) as all_ids,
-    `development_catalog`.`dbt-schema`.zzz_teams as t
+    `{{ var('dbt_catalog') }}`.`dbt-schema`.zzz_teams as t
   where
     all_ids.home_team_id = t.team_id
 ) as visitor_ids,
-  `development_catalog`.`dbt-schema`.zzz_teams as t
+  `{{ var('dbt_catalog') }}`.`dbt-schema`.zzz_teams as t
 where
   visitor_ids.visitor_team_id = t.team_id
 order by game_date desc
+
